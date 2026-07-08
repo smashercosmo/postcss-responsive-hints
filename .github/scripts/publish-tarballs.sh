1d53b1070c745508c199cf1d0e4797eef974b769
Vladislav Shkodin <smashercosmo@gmail.com>
2026-07-08 14:34:19 +0300

    chore: get tarballs paths from publish-plan.json (#65)
    * chore: get tarballs paths from publish-plan.json
    
    * docs(changeset): Get tarballs paths from publish-plan.json
    
    * fix: composite actions paths
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ .github/scripts/publish-tarballs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd tarballs
+jq -r '.plan[][] | [.name, .version, .tarball.path] | @tsv' publish-plan.json | \
+while IFS=$'\t' read -r name version tarball_path; do
+  echo "::notice:: Publishing $name@$version"
+  pnpm publish "$tarball_path" \
+  --no-git-checks \
+  --access public \
+  --provenance
+done
